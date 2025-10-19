3a310b435ab4f4eeda90c92f43a961dbf574a61d
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-10-19 23:11:21 +0200

    sw-44-collapsible-sidebar (#7)
    * sidebar
    
    * added collapsible sidebar
    
    * cleanup
    
    ---------
    
    Co-authored-by: karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,27 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
+import {AppSidebar} from "@/components/app-sidebar.tsx";
+import {SidebarProvider} from "@/components/ui/sidebar.tsx";
+import {Outlet, createRootRoute} from "@tanstack/react-router";
+import {TanStackRouterDevtoolsPanel} from "@tanstack/react-router-devtools";
+import {TanStackDevtools} from "@tanstack/react-devtools";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<Outlet />
-			<TanStackDevtools
-				config={{
-					position: "bottom-right",
-				}}
-				plugins={[
-					{
-						name: "Tanstack Router",
-						render: <TanStackRouterDevtoolsPanel />,
-					},
-				]}
-			/>
-		</>
-	),
+    component: () => (
+        <>
+            <SidebarProvider>
+                <AppSidebar/>
+                <Outlet/>
+                <TanStackDevtools
+                    config={{
+                        position: "bottom-right",
+                    }}
+                    plugins={[
+                        {
+                            name: "Tanstack Router",
+                            render: <TanStackRouterDevtoolsPanel/>,
+                        },
+                    ]}
+                />
+            </SidebarProvider>
+        </>
+    ),
+});
