0a0368f27629307f549d7942ae11f39f2b012f65
karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>
2025-10-21 19:46:36 +0200

    Sw 42: conneting login from front to back (#16)
    * sw-42: login is setting to backend
    
    * sw-42: conneting login from front to back
    
    * fix structre
    
    ---------
    
    Co-authored-by: Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>

--- src/components/assets/assets-table/assets-table.tsx
+++ src/components/assets/assets-table/assets-table.tsx
@@ -1,25 +1,16 @@
-import {useColumns} from "@/components/assets/assets-table/hooks/use-columns.tsx";
-import {DataTable} from "@/components/ui/data-table/data-table.tsx";
-import {useNavigate} from "@tanstack/react-router";
-import type {FC} from "react";
-import type {Asset} from "@/types/asset.ts";
+import { useColumns } from "@/components/assets/assets-table/hooks/use-columns.tsx";
+import { DataTable } from "@/components/ui/data-table/data-table.tsx";
+import type { FC } from "react";
+import type { Asset } from "@/types/asset.ts";
 
 interface AssetsTableProps {
     data: Asset[];
 }
 
-export const AssetsTable: FC<AssetsTableProps> = ({data}) => {
-    const navigate = useNavigate({from: '/assets'})
+export const AssetsTable: FC<AssetsTableProps> = ({ data }) => {
     const columns = useColumns();
 
 
-    return (
-        <DataTable
-            columns={columns}
-            data={data}
-            withSearchBar={true}
-            onRowClick={(rowData) => navigate({to: `/assets/${rowData.id}`,})}
-        />
-    );
+    return <DataTable columns={columns} data={data} withSearchBar={true} />;
 };
 
