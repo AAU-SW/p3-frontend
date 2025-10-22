b6f083934a463d330613fca6340c01d8d8e7583c
Kevin Nielsen <kevinanielsen@outlook.com>
2025-10-22 18:48:08 +0200

    make sure getCurrentUser doesn't throw if call is forbidden

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,16 @@
 import type { User } from '@/types/user';
 import { api } from './axios';
+import { AxiosError } from 'axios';
 
 export async function getCurrentUser() {
-  const res = await api.get<User>('/api/users/me'); // Or your own endpoint
-  return res.data;
+  try {
+    const res = await api.get<User>('/api/users/me'); // Or your own endpoint
+    return res.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        return undefined;
+      }
+    }
+  }
 }
