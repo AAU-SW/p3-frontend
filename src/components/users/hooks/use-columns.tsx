8f6c7f093db0c8ffd098cf449a0c704208dade9b
Mathildearntklov <arntklow@gmail.com>
2025-10-25 17:31:56 +0200

    sw-57: Setup users table (#15)
    * added asset table
    
    * removed package-lock
    
    * fix
    
    * add asset button
    
    * added users table
    
    * fix
    
    * fixes
    
    * run format
    
    * fix
    
    ---------
    
    Co-authored-by: Mathilde Arntkløv Stanley Petersen <mathildepetersen@MacBook-Air-tilhrende-Mathilde.local>
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>

---
+++ src/components/users/hooks/use-columns.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import type { ColumnDef } from '@tanstack/react-table';
+import { useMemo } from 'react';
+import type { User } from '@/types/user.ts';
+
+export const useColumns = () => {
+  const columns = useMemo<ColumnDef<User>[]>(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+      },
+      {
+        accessorKey: 'id',
+        header: 'ID',
+      },
+      {
+        accessorKey: 'email',
+        header: 'Email',
+      },
+      {
+        accessorKey: 'role',
+        header: 'Role',
+      },
+    ],
+    [],
+  );
+
+  return columns;
+};
