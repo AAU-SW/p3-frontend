651e16d4dc413208b0048c8a2d37e325a2dce50d
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-10-28 13:08:56 +0100

    format date

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,15 @@
+export function formatDate(input: string | Date): string {
+    let date: Date;
+
+    if (input instanceof Date) {
+        date = input;
+    } else if (typeof input == "string") {
+        const [day, month, year] = input.split("/");
+        date = new Date (`${year}-${month}-${day}`);
+    } else {
+        throw new Error("Invalid date input"); 
+    }
+
+    return date.toISOString().split("T")[0];
+}
+
