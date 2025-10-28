6d1f6c0db1723dfa9db3579578a9a2ab055c4e38
Mathildearntklov <arntklow@gmail.com>
2025-10-28 13:41:28 +0100

    format fix

--- src/components/status-badge.tsx
+++ src/components/status-badge.tsx
@@ -1,19 +1,23 @@
-import type { Asset } from "@/types/asset";
-import type { Case } from "@/types/case";
-import type { FC } from "react";
+import type { Asset } from '@/types/asset';
+import type { Case } from '@/types/case';
+import type { FC } from 'react';
 
-  
-  type StatusBadgeProps = {
-    status: Asset["status"] | Case["status"];
-  };
+type StatusBadgeProps = {
+  status: Asset['status'] | Case['status'];
+};
 
-  export const StatusBadge: FC<StatusBadgeProps> = ({ status }) => {
+export const StatusBadge: FC<StatusBadgeProps> = ({ status }) => {
   const statusColor =
     status === 'ACTIVE'
       ? 'bg-green-100 text-green-800 border-green-300'
       : status === 'CLOSED'
         ? 'bg-red-100 text-red-800 border-red-300'
         : 'bg-gray-100 text-gray-800 border-gray-300';
-    return <span className={`px-2 py-1 rounded-md text-sm font-medium border ${statusColor}`}>{status}</span>;
-
-  }
+  return (
+    <span
+      className={`px-2 py-1 rounded-md text-sm font-medium border ${statusColor}`}
+    >
+      {status}
+    </span>
+  );
+};
--- src/routes/cases/$id/index.tsx
+++ src/routes/cases/$id/index.tsx
@@ -12,7 +12,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from '@/components/ui/table';
 import { SquarePen } from 'lucide-react';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { StatusBadge } from '@/components/status-badge';
@@ -31,7 +31,6 @@ const caseData = [
   },
 ];
 
-
 const informationData = [
   {
     name: 'Pillar A/S',
@@ -48,36 +47,40 @@ function RouteComponent() {
       <h1 className="text-4xl mb-4"> Tracking device installation </h1>
 
       <div className="flex justify-between gap-4">
-      {/* case tabel*/}
-      <div className="bg-[#FFFFFF] w-2/3">
-        <div className="bg-[#01204B] text-white p-4 text-xl"> Task</div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>User</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Case</TableHead>
-              <TableHead>Asset</TableHead>
-              <TableHead></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <Avatar>
-                  <AvatarFallback>{caseData[0].user}</AvatarFallback>
-                </Avatar>
+        {/* case tabel*/}
+        <div className="bg-[#FFFFFF] w-2/3">
+          <div className="bg-[#01204B] text-white p-4 text-xl"> Task</div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>User</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Case</TableHead>
+                <TableHead>Asset</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Avatar>
+                    <AvatarFallback>{caseData[0].user}</AvatarFallback>
+                  </Avatar>
                 </TableCell>
-              <TableCell><StatusBadge status={caseData[0].status}></StatusBadge></TableCell>
-              <TableCell>{caseData[0].case}</TableCell>
-              <TableCell>{caseData[0].asset}</TableCell>
-              <TableCell><SquarePen size={16} /></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+                <TableCell>
+                  <StatusBadge status={caseData[0].status}></StatusBadge>
+                </TableCell>
+                <TableCell>{caseData[0].case}</TableCell>
+                <TableCell>{caseData[0].asset}</TableCell>
+                <TableCell>
+                  <SquarePen size={16} />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
 
-      {/* basic information box */}
+        {/* basic information box */}
 
         <Card className="w-1/3">
           <CardHeader>
@@ -85,15 +88,25 @@ function RouteComponent() {
           </CardHeader>
           <CardContent>
             <p>Name: </p>
-            <p className="border px-2 py-1 rounded">{informationData[0].name}</p>
+            <p className="border px-2 py-1 rounded">
+              {informationData[0].name}
+            </p>
             <p>Adress: </p>
-            <p className="border px-2 py-1 rounded">{informationData[0].adress}</p>
+            <p className="border px-2 py-1 rounded">
+              {informationData[0].adress}
+            </p>
             <p>Case ID: </p>
-            <p className="border px-2 py-1 rounded">{informationData[0].caseID}</p>
+            <p className="border px-2 py-1 rounded">
+              {informationData[0].caseID}
+            </p>
             <p>Customer: </p>
-            <p className="border px-2 py-1 rounded">{informationData[0].Customer}</p>
+            <p className="border px-2 py-1 rounded">
+              {informationData[0].Customer}
+            </p>
             <p>Information: </p>
-            <p className="border px-2 py-1 rounded">{informationData[0].information}</p>
+            <p className="border px-2 py-1 rounded">
+              {informationData[0].information}
+            </p>
           </CardContent>
         </Card>
       </div>
