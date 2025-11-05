5b8c85b6e16c42b717f5ffbcf9aa0920b748a0e1
tobiasandersen98 <karl.tobias.andersen@gmail.com>
2025-11-05 14:50:54 +0100

    eslint fix

--- src/routes/orders/index.tsx
+++ src/routes/orders/index.tsx
@@ -10,7 +10,7 @@ export const Route = createFileRoute('/orders/')({
 });
 
 function RouteComponent() {
-  const [ordersData, setOrdersData] = useState<Array<Order>>();
+  const [ordersData, setOrdersData] = useState<Order[]>();
 
   useEffect(() => {
     const fetchAllOrders = async () => {
