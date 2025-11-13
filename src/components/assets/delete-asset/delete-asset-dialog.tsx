73c5928f282b6038acef785fa554711873e8781e
Elias Razi <Kontakt.eliasrazi@gmail.com>
2025-11-13 19:16:11 +0100

    Added button for deleting assets

---
+++ src/components/assets/delete-asset/delete-asset-dialog.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { toast } from 'sonner';
+import { deleteAssetById } from '@/api/assets.ts';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+
+interface DeleteAssetDialogProps {
+  assetId: string;
+  onDeleteSuccess: () => void;
+}
+
+export const DeleteAssetDialog: React.FC<DeleteAssetDialogProps> = ({
+  assetId,
+  onDeleteSuccess,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await deleteAssetById(assetId);
+      onDeleteSuccess();
+      setOpen(false);
+    } catch {
+      toast.error('Failed to delete asset');
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="destructive">Delete Asset</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogTitle>Delete Asset</DialogTitle>
+        <form onSubmit={handleDelete}>
+          <p>Are you sure you want to delete this asset?</p>
+          <DialogFooter className="sm:justify-start md:justify-between pt-6">
+            <DialogClose asChild>
+              <Button variant="outline" type="button">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button type="submit" variant="destructive">
+              Delete Asset
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/components/assets/detail-header.tsx
+++ src/components/assets/detail-header.tsx
@@ -2,18 +2,27 @@ import type { FC } from 'react';
 import type { Asset } from '@/types/asset';
 import { CreateCasesDialog } from '@/components/cases/create-cases/create-cases-dialog.tsx';
 import { UpdateAssetDialog } from '@/components/assets/update-asset/update-asset-dialog.tsx';
+import { DeleteAssetDialog } from '@/components/assets/delete-asset/delete-asset-dialog.tsx';
 
 interface DetailHeaderProps {
   assetData: Asset;
 }
 
 export const DetailHeader: FC<DetailHeaderProps> = ({ assetData }) => {
+  const onDeleteSuccess = () => {
+    window.location.href = '/assets';
+  };
+
   return (
     <div className="flex items-center p-4 pb-0 justify-between gap-2">
       <h1 className="text-4xl font-medium">{assetData.name}</h1>
       <div className=" flex gap-3">
         <CreateCasesDialog />
         <UpdateAssetDialog assetData={assetData} />
+        <DeleteAssetDialog
+          assetId={assetData.id}
+          onDeleteSuccess={onDeleteSuccess}
+        />
       </div>
     </div>
   );
