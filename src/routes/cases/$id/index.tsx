baf6b59caf4819a09e360d5788612fa8587bfaa0
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-11-13 13:32:38 +0100

    sw-126: File-item visualisation + file upload capability connected to case (#48)
    * item component
    
    * file card
    
    * File header
    
    * created api method
    
    * lint
    
    * file indication on upload
    
    * format
    
    * fomrat
    
    * fix issue
    
    * lint
    
    * sw-142: fetch specific case files (#49)
    
    * fetch specific case files
    
    * file title
    
    * fix

--- src/routes/cases/$id/index.tsx
+++ src/routes/cases/$id/index.tsx
@@ -1,7 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useParams } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import type { Image } from '@/types/image.ts';
 import { CasesDetailTable } from '@/components/cases/cases-detail-table/cases-detail-table.tsx';
 import { InformationBox } from '@/components/cases/cases-detail-table/information-box.tsx';
 import BackLink from '@/components/backlink.tsx';
+import { getAllCaseFilesById } from '@/api/cases.ts';
+import { FileCard } from '@/components/file-upload/file-card.tsx';
 
 export const Route = createFileRoute('/cases/$id/')({
   component: RouteComponent,
@@ -16,6 +21,22 @@ const informationData = {
 };
 
 function RouteComponent() {
+  const params = useParams({ from: '/cases/$id/' });
+  const [caseFiles, setCaseFiles] = useState<Image[]>([]);
+  useEffect(() => {
+    const fetchAllCaseFiles = async () => {
+      try {
+        const response = await getAllCaseFilesById(params.id);
+        setCaseFiles(response);
+      } catch (error) {
+        toast.error('Failed to fetch files');
+        console.error('Failed to fetch case files:', error);
+      }
+    };
+
+    fetchAllCaseFiles();
+  }, []);
+
   return (
     <div className="w-full bg-[#F8FAFC] p-5">
       <BackLink />
@@ -32,6 +53,7 @@ function RouteComponent() {
         {/* information box */}
         <div className="w-1/3">
           <InformationBox informationData={informationData} />
+          <FileCard image={caseFiles} />
         </div>
       </div>
     </div>
