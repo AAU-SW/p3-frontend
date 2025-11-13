511c0cc6e341fa97d01d9e5d14aa7a0f773cc378
Elias Razi <Kontakt.eliasrazi@gmail.com>
2025-11-13 20:00:26 +0100

    Ran format

--- src/components/assets/delete-asset/delete-asset-dialog.tsx
+++ src/components/assets/delete-asset/delete-asset-dialog.tsx
@@ -11,7 +11,6 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 
-
 interface DeleteAssetDialogProps {
   assetId: string;
   onDeleteSuccess: () => void;
