9c745a7eec739ce6ebc2cb9a1c957433c4a6b164
karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>
2025-11-14 12:38:37 +0100

    sw-132: add a customer view and a creat butten (#50)
    * sw-132: add a customer view and a creat butten
    
    * fix: added an extra condition
    
    * fix comment
    
    * Update src/api/customer.ts
    
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>
    
    * Update src/api/customer.ts
    
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>
    
    * build
    
    ---------
    
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>
    Co-authored-by: Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>

--- src/api/customer.ts
+++ src/api/customer.ts
@@ -1,6 +1,6 @@
 import { api } from './axios';
 import type { Case } from '@/types/case';
-import type { Customer } from '@/types/customer.ts';
+import type { CreateCustomer, Customer } from '@/types/customer.ts';
 
 export async function getCustomers() {
   const res = await api.get(`api/customers`);
@@ -9,6 +9,24 @@ export async function getCustomers() {
 
 export async function getCustomerById(customerId: string) {
   const res = await api.get<Customer>(`api/customers/${customerId}`);
+  return res.data;
+}
+
+export async function createCustomer(data: CreateCustomer, imageFile?: File) {
+  if (imageFile && imageFile.size != 0) {
+    const formData = new FormData();
+    formData.append('customer', JSON.stringify(data));
+    formData.append('image', imageFile as Blob);
+
+    const res = await api.post(`/api/customers`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return res.data;
+  }
+
+  const res = await api.post(`/api/customers`, data);
   return res.data;
 }
 
@@ -21,11 +39,6 @@ export async function updateCustomerById(
 }
 
 export async function deleteCustomerById(customerId: string) {
-  const res = await api.delete<Customer>(`api/customers/${customerId}`);
-  return res.data;
-}
-
-export async function createCustomer(data: Case) {
-  const res = await api.post(`/api/customers`, data);
+  const res = await api.delete(`api/customers/${customerId}`);
   return res.data;
 }
