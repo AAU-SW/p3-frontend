188267e789abc76a0b381946b8e6bdf13c7e479d
tobiasandersen98 <karl.tobias.andersen@gmail.com>
2025-11-19 14:13:12 +0100

    Sw-149: add discreption on the asset

--- src/components/assets/detail-header.tsx
+++ src/components/assets/detail-header.tsx
@@ -4,6 +4,7 @@ import type { Asset } from '@/types/asset';
 import { CreateCasesDialog } from '@/components/cases/create-cases/create-cases-dialog.tsx';
 import { UpdateAssetDialog } from '@/components/assets/update-asset/update-asset-dialog.tsx';
 import { DeleteAssetDialog } from '@/components/assets/delete-asset/delete-asset-dialog.tsx';
+import { Label } from '@/components/ui/label.tsx';
 
 interface DetailHeaderProps {
   assetData: Asset;
@@ -18,15 +19,21 @@ export const DetailHeader: FC<DetailHeaderProps> = ({ assetData }) => {
   };
 
   return (
-    <div className="flex items-center p-4 pb-0 justify-between gap-2">
-      <h1 className="text-4xl font-medium">{assetData.name}</h1>
-      <div className=" flex gap-3">
-        <CreateCasesDialog />
-        <UpdateAssetDialog assetData={assetData} />
-        <DeleteAssetDialog
-          assetId={assetData.id}
-          onDeleteSuccess={onDeleteSuccess}
-        />
+    <div className="p-4">
+      <div className="flex items-center pb-0 justify-between ">
+        <h1 className="text-4xl font-medium">{assetData.name}</h1>
+        <div className=" flex gap-3">
+          <CreateCasesDialog />
+          <UpdateAssetDialog assetData={assetData} />
+          <DeleteAssetDialog
+            assetId={assetData.id}
+            onDeleteSuccess={onDeleteSuccess}
+          />
+        </div>
+      </div>
+      <div className="pt-4">
+        <Label className="mb-2 text-gray-500">Description</Label>
+        {assetData.description}
       </div>
     </div>
   );
