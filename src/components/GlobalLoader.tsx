b46b90d358087b203b83bf6d88eef96697218c73
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-11-22 15:07:55 +0100

    format

--- src/components/GlobalLoader.tsx
+++ src/components/GlobalLoader.tsx
@@ -1,8 +1,9 @@
-import { Spinner } from "@/components/ui/spinner"
+import { Spinner } from '@/components/ui/spinner';
+
 export const GlobalLoader = () => {
-    return (
-        <div className="grid place-items-center w-full h-full">
-            <Spinner className="size-16" />
-        </div>
-    );
+  return (
+    <div className="grid place-items-center w-full h-full">
+      <Spinner className="size-16" />
+    </div>
+  );
 };
