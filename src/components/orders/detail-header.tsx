55332130de5303b3c1c73ad01131f78ef68851b8
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-11-22 17:31:42 +0100

    Removed h1 in detail header

--- src/components/orders/detail-header.tsx
+++ src/components/orders/detail-header.tsx
@@ -3,7 +3,6 @@ import { CreateOrderDialog } from '@/components/orders/create-order/create-order
 export const DetailHeader = () => {
   return (
     <div className="flex items-center p-4 pb-0 justify-between gap-2">
-      <h1 className="text-4xl font-medium"></h1>
       <div className=" flex gap-3">
         <CreateOrderDialog />
       </div>
