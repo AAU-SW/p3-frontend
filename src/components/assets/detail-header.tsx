dea40e8d4e4fd2a94beb06b3af5ccc8d45c352ad
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-11-23 20:34:47 +0100

    Sw 150 (#75)
    * tilføjet add order butten
    
    * run lint --fix
    
    * lavet add order button
    
    * fiksede orderNumber
    
    * created add button to orders
    
    * button order
    
    * fix: eslint error
    
    * fjernet optinal og l. 80-83
    
    * removed optional and l. 80-83
    
    * fixup! button order
    
    * Removed h1 in detail header
    
    * creation for cases
    
    * fixed import
    
    * fixed build problem
    
    * fixed onCreated build
    
    * fc removed
    
    * pnpm format
    
    * fixed newCase
    
    * fix case creation from asset page
    
    * format
    
    * format and removed updating case list
    
    ---------
    
    Co-authored-by: karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>
    Co-authored-by: tobiasandersen98 <karl.tobias.andersen@gmail.com>
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>
    Co-authored-by: Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>

--- src/components/assets/detail-header.tsx
+++ src/components/assets/detail-header.tsx
@@ -1,15 +1,20 @@
 import { useNavigate } from '@tanstack/react-router';
 import type { FC } from 'react';
 import type { Asset } from '@/types/asset';
+import type { Case } from '@/types/case';
 import { CreateCasesDialog } from '@/components/cases/create-cases/create-cases-dialog.tsx';
 import { UpdateAssetDialog } from '@/components/assets/update-asset/update-asset-dialog.tsx';
 import { DeleteAssetDialog } from '@/components/assets/delete-asset/delete-asset-dialog.tsx';
 
 interface DetailHeaderProps {
   assetData: Asset;
+  onCaseCreated: (newCase: Case) => void;
 }
 
-export const DetailHeader: FC<DetailHeaderProps> = ({ assetData }) => {
+export const DetailHeader: FC<DetailHeaderProps> = ({
+  assetData,
+  onCaseCreated,
+}) => {
   const navigate = useNavigate();
   const onDeleteSuccess = () => {
     navigate({
@@ -22,7 +27,7 @@ export const DetailHeader: FC<DetailHeaderProps> = ({ assetData }) => {
       <div className="flex items-center pb-0 justify-between ">
         <h1 className="text-4xl font-medium">{assetData.name}</h1>
         <div className=" flex gap-3">
-          <CreateCasesDialog />
+          <CreateCasesDialog onCreated={onCaseCreated} />
           <UpdateAssetDialog assetData={assetData} />
           <DeleteAssetDialog
             assetId={assetData.id}
