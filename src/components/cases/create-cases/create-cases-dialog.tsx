567ad497c1bb006dc0c79f1963ae0ba663f0c614
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-11-23 11:04:47 +0100

    fc removed

--- src/components/cases/create-cases/create-cases-dialog.tsx
+++ src/components/cases/create-cases/create-cases-dialog.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
-import type { FC, FormEvent } from 'react';
+import type { FormEvent } from 'react';
 import type { CreateCase } from '@/types/case.ts';
 import type { Asset } from '@/types/asset.ts';
 import type { User } from '@/types/user.ts';
